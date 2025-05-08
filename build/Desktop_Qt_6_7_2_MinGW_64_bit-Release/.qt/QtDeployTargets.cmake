9c044f5bc08d57f8b2e59ddc16a805ece9610c8f
panyf <panyf0819@qq.com>
2025-05-08 10:52:27 +0800

    [Feature]建立了仓库，修改ico

---
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Release/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_pChat_FILE D:/code/pChat/build/Desktop_Qt_6_7_2_MinGW_64_bit-Release/pChat.exe)
+set(__QT_DEPLOY_TARGET_pChat_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_pChat_RUNTIME_DLLS D:/software/QT/6.7.2/mingw_64/bin/Qt6Widgets.dll;D:/software/QT/6.7.2/mingw_64/bin/Qt6Gui.dll;D:/software/QT/6.7.2/mingw_64/bin/Qt6Core.dll)
